36584189eaf9cdd75baeb6025548d4a3324f69c4
neko2k9 <neko2k9@hotmail.com>
2019-04-01 23:48:27 -0400

    Functional registration, login, logout pages

--- CapstoneProject/urls.py
+++ CapstoneProject/urls.py
@@ -17,8 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from website.views import hello
+from website.views import home
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', hello, name='hello'),
+    url(r'^$', hello, name='index'),
+    path('register/', user_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('home/', home, name="home"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 def hello(request):
    return render(request, "index.html", {})
 
+@login_required
+def home(request):
+   return render(request, "home.html", {})
