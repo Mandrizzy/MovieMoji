6e79b4704b128f6385a5a6e85bf88bc1f5b991e8
Theodore Coley <theodorecoley659@gmail.com>
2019-04-07 00:24:35 -0600

    updated sqlite database and changed theme for login and sign up page

---
+++ RecommendationSystem/recommender.py
@@ -0,0 +1,2 @@
+movieGenreList = ["action", "adventure", "animation", "children's", "comedy", "crime", "documentary", "drama", "fantasy", "film-noir", "horror", "musical", "mystery", "romance", "sci-fy", "thriller", "war", "western"]
+
