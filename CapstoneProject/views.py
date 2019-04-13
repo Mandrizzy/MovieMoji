f859c048b25e228fbc8f6c12f8e0b9f09c3387fa
Theodore Coley <theodorecoley659@gmail.com>
2019-04-13 17:06:31 -0600

    refactor some files in capstone project

--- CapstoneProject/views.py
+++ CapstoneProject/views.py
@@ -2,9 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-def hello(request):
-   return render(request, "dashboard.html", {})
+
+def index(request):
+    return render(request, "landing.html", {})
+
 
 @login_required
 def home(request):
-   return render(request, "home.html", {})
+    return render(request, "dashboard.html", {})
+
